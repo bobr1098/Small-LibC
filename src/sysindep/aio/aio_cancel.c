15320aacad0b52c888a756860a68cbf84d3d59e5
Polina <aalx2176@gmail.com>
2026-03-24 12:49:48 +0300

    fix: aio_cancel signature

--- src/sysindep/aio/aio_cancel.c
+++ src/sysindep/aio/aio_cancel.c
@@ -2,6 +2,6 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int aio_cancel(struct aiocb *aiocbp) {
-    return (int)syscall(SYS_aio_cancel, ((long)(aiocbp->aio_fildes)), (long)aiocbp);
+int aio_cancel(int fildes, struct aiocb *aiocbp) {
+    return (int)syscall(SYS_aio_cancel, (long)fildes, (long)aiocbp);
 }
